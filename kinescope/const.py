0febf8f77647ba9989fed050893c4e4dff08d1eb
face <96299006+Sti11ness@users.noreply.github.com>
2025-08-28 17:49:43 +0300

    Update const.py
    changed vars for DASH | HLS

--- kinescope/const.py
+++ kinescope/const.py
@@ -1,5 +1,12 @@
 KINESCOPE_BASE_URL = 'https://kinescope.io'
+
+# DASH
 KINESCOPE_MASTER_PLAYLIST_URL = 'https://kinescope.io/{video_id}/master.mpd'
+KINESCOPE_DASH_MASTER_URL = KINESCOPE_MASTER_PLAYLIST_URL
+
+# HLS 
+KINESCOPE_HLS_MASTER_URL = 'https://kinescope.io/{video_id}/master.m3u8'
+
 KINESCOPE_CLEARKEY_LICENSE_URL = 'https://license.kinescope.io/v1/vod/{video_id}/acquire/clearkey?token='
 
 DEFAULT_REFERER = 'https://kinescope.io'
